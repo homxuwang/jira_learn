023ef8254b81f4c13167a0f2737cd9a932e241f5
homxu <wanghongxu1994@qq.com>
2021-03-31 17:42:28 +0800

    chroe: react表单 Test

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@
  * @Author       : homxuwang
  * @Date         : 2021-03-25 09:20:48
  * @LastEditors  : homxuwang
- * @LastEditTime : 2021-03-31 09:24:50
+ * @LastEditTime : 2021-03-31 11:25:25
  */
 import "./App.css";
 import { ProjectListScreen } from "screens/project-list";
 import { TsReactTest } from "screens/project-list/try-use-array";
+import { LoginScreen } from "screens/login";
 
 function App() {
   return (
     <div className="App">
       {/* <ProjectListScreen /> */}
-      <TsReactTest />
+      {/* <TsReactTest /> */}
+      <LoginScreen/>
     </div>
   );
 }
---
+++ src/screens/login/index.tsx
@@ -0,0 +1,48 @@
+/*
+ * @Description  : 
+ * @version      : 
+ * @Author       : homxuwang
+ * @Date         : 2021-03-31 09:52:11
+ * @LastEditors  : homxuwang
+ * @LastEditTime : 2021-03-31 16:23:36
+ */
+import React, { FormEvent, FormEventHandler } from 'react';
+
+const apiUrl = process.env.REACT_APP_API_URL
+
+export const LoginScreen = () => {
+    const login = (param:{username:string, password:string}) => {        
+        fetch(`${apiUrl}/login`,{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(param)
+        }).then(async response => {
+            if(response.ok){
+                console.log(response.json())
+            }
+        })
+    }
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        //阻止表单提交的默认行为
+        event.preventDefault()
+        const username = (event.currentTarget.elements[0] as HTMLFormElement).value
+        const password = (event.currentTarget.elements[1] as HTMLFormElement).value
+
+        login({username,password} );
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="username">用户名</label>
+                <input type="text" id={'username'}/>
+            </div>
+            <div>
+                <label htmlFor="password">密码</label>
+                <input type="password" id={'password'}/>
+            </div>
+            <button type="submit">登录</button>
+        </form>
+    )
+}
